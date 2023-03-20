909b7fe900cbfd9f7f0912261c2e8af35fa15a56
Bealko <61605695+Bealko@users.noreply.github.com>
2023-03-20 16:40:35 +0200

    Initial commit

---
+++ emojiondb.js
@@ -0,0 +1,7 @@
+
+const cursors = {
+    drag : String.fromCodePoint(129306), //EMOJI BACKHAND
+    pointer : String.fromCodePoint(128070), //EMOJI FINGER POINTING
+    editLine : String.fromCodePoint(9997) + String.fromCodePoint(65039), //EMOJI HOLDING PEN
+}
+export default cursors;
